c04d4fc02a1c93f9c6d10ad5a6673998e3fb0730
uniflguertas <149299894+uniflguertas@users.noreply.github.com>
2023-10-29 07:30:02 -0300

    Limitação do número máximo de clientes e declaração de variáveis

--- main.c
+++ main.c
@@ -2,5 +2,12 @@
 #include "proj.h"
 
 int main(){
+ //  Limite do número de clientes cadastráveis:
+    Cliente clientes[1000];
+//  Variável que determina o número de clientes cadastrados:
+    int numClientes = 0;
+//  Variável usada para detectar qual opção será utilizada:
+    int opcao;
+
   
 }
